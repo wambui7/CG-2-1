6b67d376379d0864ecbf422f3db1d945cb83b925
wambui7 <41122098+wambui7@users.noreply.github.com>
2018-07-19 21:51:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.What is your name?");
+
+            Console.Write("Sheila");
+
+            var sheila = Console.ReadLine();
+
+            Console.WriteLine(string.Format("Hello {0}", sheila));
+
+            Console.Read();
+
+            
+          
+            
+
+            
+
+
+        }
+    }
+}
